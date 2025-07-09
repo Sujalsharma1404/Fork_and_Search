74214ba10263865cef19c0f5ddd66d834e92c2af
Sujalsharma1404 <suj89012@gmail.com>
2025-07-09 10:29:01 +0530

    added nescessary components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
+import Footer from "./Components/Footer";
+
+
+import About from "./Pages/About";
+
 
 function App() {
   return (
-       <Router>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+
       </Routes>
+      <Footer />
+
     </Router>
+
   );
 }
 
