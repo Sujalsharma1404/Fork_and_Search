c74b8286a85c3f473b826327da3c45d546c7a995
Sujalsharma1404 <suj89012@gmail.com>
2025-07-09 10:25:19 +0530

    imported the componets of the home page and almost complete the home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,16 @@
 import React from "react";
 import HeaderHome from "../Components/HeaderHome";
 import HomeCarousel from "../Components/HomeCarousel";
-
+import CategorySection from "../Components/CategorySection";
+import NewsletterSignup from "../Components/NewsletterSignup";
 function Home() {
   return (
     <>
       <HeaderHome />
       {/* Later: HeroSection, FeaturedRecipes, etc. */}
       <HomeCarousel/>
+      <CategorySection/>
+      <NewsletterSignup />
     </>
   );
 }
