f36e55bbdffd1afca76fe472c90a20cb0eb7072b
Sujalsharma1404 <suj89012@gmail.com>
2025-07-16 12:40:55 +0530

    Scroll to top when routing to the pages

---
+++ src/ScrollToTop.js
@@ -0,0 +1,17 @@
+// src/ScrollToTop.js
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "instant"  // or "smooth" if you want smooth scroll
+    });
+  }, [pathname]);
+
+  return null;
+}
