bb10c74bd47931f69878b0b388f09f710e98c073
Sujalsharma1404 <suj89012@gmail.com>
2025-07-16 12:40:12 +0530

    Modified the page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home";
-import Footer from "./Components/Footer";
-import Contact from "./Pages/Contact";
-import Header from "./Components/Header";
-
-
-import About from "./Pages/About";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Contact from './Pages/Contact';
+import About from './Pages/About';
+import RecipesPage from './Pages/RecipesPage';
+import CategoryPage from './Pages/CategoryPage';
+import DishDetail from './Pages/DishDetail';
+import Footer from './Components/Footer';
+import ScrollToTop from "./ScrollToTop";
 
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact" element={< Contact />} />
         <Route path="/about" element={<About />} />
-         <Route path="/contact" element={<Contact />} />
+        <Route path="/recipes" element={<RecipesPage />} />
 
+        <Route path="/category/:categoryName" element={<CategoryPage />} />
+        <Route path="/dish/:dishId" element={<DishDetail />} />
       </Routes>
       <Footer />
 
